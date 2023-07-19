4270d80538482dd4108d0fb46d936bef752018e5
imuryu <imuryu3212@gmail.com>
2023-07-19 22:04:52 +0900

    セーブ

--- MumChaApp/forms.py
+++ MumChaApp/forms.py
@@ -5,10 +5,10 @@ class login_form(forms.Form):
   email = forms.EmailField(label='', widget= forms.EmailInput(attrs={'placeholder':'email'}), required=True)
   password = forms.CharField(label='', widget= forms.PasswordInput(attrs={'placeholder':'Password'}), required=True)
 
-class register_form(form.Form):
+class register_form(forms.Form):
   email = forms.CharField(label = '', widget = forms.EmailInput(attrs={'placeholder':'メールアドレス'}), required = True)
-  password = forms.CharField(label = '', widget = forms.TextInput(attrs={'placeholder':'パスワード'}), required = True)
+  password = forms.CharField(label = '', widget = forms.PasswordInput(attrs={'placeholder':'パスワード'}), required = True)
   name = forms.CharField(label = '', widget = forms.TextInput(attrs={'placeholder':'ユーザーネーム'}), required = True)
-  img = forms.ImageField(label = '', widget = forms.ImageInput(attrs={'placeholder':'画像を選択'}))
-  birth = forms.DateField(label = '', widget = forms.DateInput(attrs={'placeholder':'誕生日'}))
+  img = forms.ImageField(label = '', widget = forms.FileInput(attrs={'placeholder':'画像を選択'}))
+  birth = forms.DateField(label = '', widget = forms.NumberInput(attrs={'placeholder':'誕生日'}))
   comment = forms.CharField(label = '', widget = forms.TextInput(attrs={'placeholder':'コメント'}))
--- MumChaApp/urls.py
+++ MumChaApp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.firstlogin), #トップページ表示
     # path('MumlaChat/', views.MumlaChat), #トップページ表示
     path('login/', views.login), #ログイン
+    path('register/', views.register), #会員登録フォーム
     path('registration/', views.registration), #会員登録
     # path('logout/', views.logout), #ログアウト
 ]

1b31ea7c996b4428e3ed8243ea6973ea2de66c12
te94d <te94da@gmail.com>
2023-07-21 00:33:30 +0900

    fix 画像以外の投稿

--- MumChaApp/urls.py
+++ MumChaApp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('register/', views.register), #会員登録フォーム
     path('registration/', views.registration), #会員登録
     path('logout/', views.logout), #ログアウト
-    #path('home/', views.home), #ホーム表示
+    path('home/', views.home), #ホーム表示
     path('tweet/', views.tweet), #投稿
     path('tweeting/', views.tweeting), #投稿処理
 ]
